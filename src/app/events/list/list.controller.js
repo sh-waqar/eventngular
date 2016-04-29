898349e897048b11b353049a226c6d3fade49217
Waqar Ali <waqar.ali911@gmail.com>
2016-04-29 14:21:24 +0500

    Added event pages (create, list, detail).

--- src/app/events/list/list.controller.js
+++ src/app/events/list/list.controller.js
@@ -6,8 +6,44 @@
         .controller('ListController', ListController);
 
     /** @ngInject */
-    function ListController() {
+    function ListController($uibModal) {
         var vm = this;
 
+        vm.eventList = [{
+            name: 'Event 1',
+            location: 'Dubai, UAE',
+            datetime: '10 Jan 2016',
+            attendies: 22
+        }, {
+            name: 'Event 1',
+            location: 'Dubai, UAE',
+            datetime: '10 Jan 2016',
+            attendies: 22
+        }, {
+            name: 'Event 1',
+            location: 'Dubai, UAE',
+            datetime: '10 Jan 2016',
+            attendies: 22
+        }, {
+            name: 'Event 1',
+            location: 'Dubai, UAE',
+            datetime: '10 Jan 2016',
+            attendies: 22
+        }, {
+            name: 'Event 1',
+            location: 'Dubai, UAE',
+            datetime: '10 Jan 2016',
+            attendies: 22
+        }];
+
+        vm.createEvent = function() {
+
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'app/events/create/create.html',
+                controller: 'CreateController',
+                size: 'large'
+            });
+        };
     }
 })();
